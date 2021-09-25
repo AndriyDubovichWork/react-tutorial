0396a11bf4ddb0c83805be7af6c3eb2a59463b8a
Andrew <66020477+asadad2281@users.noreply.github.com>
2021-09-25 13:14:07 +0300

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div className = 'wrapper'> 
-
+      <h1 class='text'>Hello</h1>
+      <h3 class='text'>I am hacker</h3>
+      <h5 class='text'>I mean realy hacker</h5>
     </div>
   );
 }
