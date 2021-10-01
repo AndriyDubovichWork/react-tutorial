66f04a7c4259d44e958a572bf3bf0ffdd61d6c63
Andrew <66020477+asadad2281@users.noreply.github.com>
2021-10-01 21:27:27 +0300

    added main pics

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 function App() {
   return (
-    <div className = 'wrapper'> 
-      <h1 class='text'>Hello</h1>
-      <h3 class='text'>I am hacker</h3>
-      <h5 class='text'>I mean realy hacker</h5>
+
+
+    <div id = 'Site'> 
+      <div id='header'>
+        <h1>Concern AK</h1>
+      </div>
+        <div id = 'main'>
+          <div id = 'first_img'>
+            <img src='imgs/ak19.jpg' alt="ak19"/>
+            <h1>AK-19</h1>
+            <h3>.223/7.62*39</h3>
+          </div>
+        </div>
+
     </div>
   );
 }
